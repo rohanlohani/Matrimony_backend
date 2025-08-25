7c778f186a90578d939c0bfc83813aeb9d72e20f
Suprabha47 <143326844+Suprabha47@users.noreply.github.com>
2025-08-25 15:50:20 +0530

    updated server file

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// index.js
 const express = require("express");
 const cors = require("cors");
 const { connectDB, sequelize } = require("./config/db");
@@ -7,36 +6,29 @@ const adminRoutes = require("./routes/authRoute");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api", adminRoutes);
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
-// Get port from environment or default to 4000
-const PORT = process.env.PORT || 4000;
+const PORT = 4005;
 
 async function init() {
   try {
-    // Connect to DB
     await connectDB();
     console.log("✅ Connected to MySQL database");
 
-    // Sync Sequelize models
     await sequelize.sync();
     console.log("✅ Database synced");
 
-    // Start Express server
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`🚀 Express server started on http://localhost:${PORT}`);
+    app.listen(PORT, () => {
+      console.log(` Express server started on http://localhost:${PORT}`);
     });
   } catch (err) {
-    console.error("❌ Error starting server:", err);
-    process.exit(1); // Exit the process on error
+    console.error("Error starting server:", err);
   }
 }
 
